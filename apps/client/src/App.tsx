2ecb6ff842bc6afec2074da443861997c6e93314
Md-Alqma <mdalqma7@gmail.com>
2026-01-20 17:53:07 +0530

    feat(setup): removed postcss config

--- apps/client/src/App.tsx
+++ apps/client/src/App.tsx
@@ -1,7 +1,7 @@
 function App() {
   return (
     <>
-      <h1 className="text-5xl text-red-500">Postgres Performance Lab</h1>
+      <h1 className="text-4xl text-red-400">Postgres Performance Lab</h1>
     </>
   )
 }
--- packages/ui/postcss.config.cjs
+++
@@ -1,1 +0,0 @@
-module.exports = require("@pg-performance-lab/config/postcss.config.cjs");
