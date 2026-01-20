0168953b9f5c521d0ded343acb4b539247225aff
Md-Alqma <mdalqma7@gmail.com>
2026-01-20 17:32:20 +0530

    feat(client): cleanup of client app

--- apps/client/src/App.tsx
+++ apps/client/src/App.tsx
@@ -1,13 +1,7 @@
-import { Button, Tooltip, TooltipContent, TooltipTrigger } from '@pg-performance-lab/ui'
 function App() {
   return (
     <>
       <h1 className="text-5xl text-red-500">Postgres Performance Lab</h1>
-      <Button variant={'danger'}>Hello</Button>
-      <Tooltip>
-        <TooltipTrigger>Hi</TooltipTrigger>
-        <TooltipContent>Hello</TooltipContent>
-      </Tooltip>
     </>
   )
 }
